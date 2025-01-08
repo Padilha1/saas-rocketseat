0a5861cc41aff10c87be979b7ce7bfb73c2120e9
Padilha1 <padilha.matheus@hotmail.com>
2025-01-08 15:46:54 -0300

    Permissions Package

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,28 @@
+import { AbilityBuilder } from '@casl/ability'
+import { AppAbility } from '.'
+import { User } from './models/user'
+import { Role } from './roles'
+
+type permissionsByRole = (
+  user: User,
+  builder: AbilityBuilder<AppAbility>
+) => void
+
+export const permissions: Record<Role, permissionsByRole> = {
+  ADMIN(user, { can, cannot }) {
+    can('manage', 'all')
+    cannot(['transfer_ownership', 'update'], 'Organization')
+    can(['transfer_ownership', 'update'], 'Organization', {
+      ownerId: { $eq: user.id },
+    })
+  },
+  MEMBER(user, { can }) {
+    // can('invite', 'User')
+    can('get', 'User')
+    can(['create', 'get'], 'Project')
+    can(['update', 'delete'], 'Project', { ownerId: { $eq: user.id } })
+  },
+  BILLING(_, { can }) {
+    can('manage', 'Billing')
+  },
+}
