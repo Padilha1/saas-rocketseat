2c943f19e0006f8175064819770b17e8d4c454cb
Padilha1 <padilha.matheus@hotmail.com>
2025-01-29 16:13:18 -0300

    frontend2

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,5 +1,42 @@
 'use server'
+import { z } from 'zod'
+import { signInWithPassword } from '@/http/sign-in-with-password'
+import { HTTPError } from 'ky'
 
-export async function signInWithEmailAndPassword(data: FormData) {
-  console.log(Object.fromEntries(data))
+const signInSchema = z.object({
+  email: z.string().email({ message: 'Please provide valid email.' }),
+  password: z.string().min(1, { message: 'Please provide valid email.' }),
+})
+
+export async function signInWithEmailAndPassword(_: unknown, data: FormData) {
+  const result = signInSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { email, password } = result.data
+
+  try {
+    const { token } = await signInWithPassword({
+      email: String(email),
+      password: String(password),
+    })
+
+    console.log(token)
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+      return { success: false, message, errors: null }
+    }
+    console.error(error)
+    return {
+      success: false,
+      message: 'Unexpected error, try again later.',
+      errors: null,
+    }
+  }
+  return { success: true, message: null, errors: null }
 }
--- apps/web/src/app/auth/sign-in/page.tsx
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -1,47 +1,5 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Separator } from '@/components/ui/separator'
-import githubIcon from '@/app/assets/github.svg'
-import Link from 'next/link'
-import Image from 'next/image'
-import { signInWithEmailAndPassword } from './actions'
+import { SignInForm } from './sign-in-form'
 
 export default function SignInPage() {
-  return (
-    <form action={signInWithEmailAndPassword} className="space-y-4">
-      <div className="space-y-1">
-        <Label htmlFor="email">E-mail</Label>
-        <Input name="email" type="email" id="email"></Input>
-      </div>
-      <div className="space-y-1">
-        <Label htmlFor="password">Password</Label>
-        <Input name="password" type="password" id="password"></Input>
-        <Link
-          href="/auth/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
-        >
-          {' '}
-          Forgot your password ?
-        </Link>
-      </div>
-
-      <Button className="w-full" type="submit">
-        Sign in with e-mail{' '}
-      </Button>
-      <Button variant={'link'} className="w-full" size={'sm'} asChild>
-        <Link href="/auth/sign-up"> Don`t have an account ? Sign Up</Link>
-      </Button>
-
-      <Separator />
-      <Button className="w-full" variant={'outline'} type="submit">
-        <Image
-          src={githubIcon}
-          className="mr-2 size-4 dark:invert"
-          alt="Github Icon"
-        />
-        Sign in with Github{' '}
-      </Button>
-    </form>
-  )
+  return <SignInForm />
 }
